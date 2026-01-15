c593616e291a5904410cee010405179868d55ace
habiba hussein <habiba.hussein@moringaschool.comclear>
2026-01-15 09:42:47 +0300

    using session object

--- server/app.py
+++ server/app.py
@@ -1,34 +1,49 @@
-#!/usr/bin/env python3
-
-from flask import Flask, make_response, jsonify, session
+from flask import Flask, jsonify, session
 from flask_migrate import Migrate
 
-from models import db, Article, User
+from models import db, Article
 
 app = Flask(__name__)
-app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
+
+# REQUIRED for sessions
+app.secret_key = "super-secret-key"
+
+# Database config
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
-
-migrate = Migrate(app, db)
 
 db.init_app(app)
+migrate = Migrate(app, db)
 
-@app.route('/clear')
+
+@app.route('/articles/<int:id>', methods=['GET'])
+def get_article(id):
+    # initialize page views if not present
+    session['page_views'] = session['page_views'] if 'page_views' in session else 0
+
+    # increment page views
+    session['page_views'] += 1
+
+    # enforce paywall
+    if session['page_views'] > 3:
+        return jsonify({
+            'message': 'Maximum pageview limit reached'
+        }), 401
+
+    # fetch article
+    article = Article.query.get(id)
+
+    if not article:
+        return jsonify({'error': 'Article not found'}), 404
+
+    return jsonify(article.to_dict()), 200
+
+
+@app.route('/clear', methods=['DELETE'])
 def clear_session():
-    session['page_views'] = 0
-    return {'message': '200: Successfully cleared session data.'}, 200
+    session.clear()
+    return {}, 204
 
-@app.route('/articles')
-def index_articles():
-
-    pass
-
-@app.route('/articles/<int:id>')
-def show_article(id):
-
-    pass
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5555, debug=True)
